f40bbc73df154bcec7a91f752e56b72a3c749916
cobra3282000 <cobra3282000@live.com>
2025-10-01 18:48:01 -0700

    fixed arch build error

--- profiledef.sh
+++ profiledef.sh
@@ -8,9 +8,8 @@ iso_application="Acreetion OS Install Media"
 iso_version="1.0"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp'
-           'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux'
+           'uefi.grub')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
